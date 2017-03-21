40993a98f13c50091aa1728fa1487d24e288dfd6
wallen <wallen_weel@outlook.com>
2017-03-21 22:40:18 +0800

    Content Script Selection Part

---
+++ src/scripts/content.js
@@ -0,0 +1,13 @@
+import Weel, { weel as $ } from "./libs/Weel"
+
+((doc) => {
+  // $(doc.body).register('mouseup', ev => {
+  //   var t = ''
+  //   function gText(e) {
+  //     t = (document.all) ? document.selection.createRange().text : document.getSelection()
+  //
+  //     document.getElementById('input').value = t
+  //   }
+  //   console.log(document.selection.createRange())
+  // })
+})(document)

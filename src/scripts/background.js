dca85502b64e9254be3f5ffb080d3a904ebc3870
wallen <wallen_weel@outlook.com>
2017-03-22 23:28:08 +0800

    Try to Use Tabs's Method Inject JS

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -4,7 +4,8 @@ import { settings } from "./libs/ui/config"
 import {
   MESSAGE_IN_BACKGROUND,
   NO_CONNECT_NAME,
-
+  CONNECT_FROM_CONTENT,
+  RESPONSE_FROM_BACKGROUND,
   CONNECT_WITH_TRANSLATING,
 } from "./libs/actions/types"
 
@@ -12,20 +13,41 @@ import translate from "./libs/services/translation"
 import "./libs/actions/background"
 
 const scope = 'background'
+const { runtime, tabs } = browser
 
 // Initial Settings
 settings().init()
 
-browser.runtime.onConnect.addListener(port => {
+runtime.onConnect.addListener(port => {
   const { name, onMessage } = port
 
   switch (name) {
 
   case CONNECT_WITH_TRANSLATING:
     return onMessage.addListener(data => do_action(MESSAGE_IN_BACKGROUND, data, port))
+
+  case CONNECT_FROM_CONTENT:
+    return settings().get(cfgs => {
+      return port.postMessage({
+        type: RESPONSE_FROM_BACKGROUND,
+        payload: cfgs,
+      })
+    })
+
   default:
     return onMessage.addListener(data => do_action(NO_CONNECT_NAME, data, port))
 
   }
+})
 
+tabs.onUpdated.addListener(function(tabId , info) {
+  if (info.status == "complete") {
+    tabs.executeScript(tabId, {
+      allFrames: true,
+      file: "js/weel-translation.js",
+      runAt: 'document_end',
+    }).then(r => {
+      console.log(r)
+    })
+  }
 })

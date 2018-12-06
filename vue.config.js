9a31ebefacea8070811a2a7f00796a1d6ccbfd8e
wallen <wallen_weel@outlook.com>
2018-12-06 20:43:53 +0800

    custom config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
+const GenerateJsonPlugin = require('generate-json-webpack-plugin')
+
 module.exports = {
   pages: {
     'background/main': 'src/pages/background.ts',
     'popup/main': 'src/pages/popup.ts',
     'options/main': 'src/pages/options.ts',
-  }
-}
+    'content/main': 'src/pages/content.ts',
+  },
+  filenameHashing: false,
+  configureWebpack: {
+    devtool: "inline-source-map",
+    plugins: [
+      new GenerateJsonPlugin('manifest.json', Object.assign(
+        require('./src/assets/manifest.base.json'),
+        require(`./src/assets/manifest.${process.env.NODE_ENV}.json`)
+      ))
+    ]
+  },
+};

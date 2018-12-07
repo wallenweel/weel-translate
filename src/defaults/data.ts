6635052d64097a843c84f9f62bfdc877409edb49
wallen <wallen_weel@outlook.com>
2018-12-08 00:18:52 +0800

    add language config

--- src/defaults/data.ts
+++ src/defaults/data.ts
@@ -1,12 +1,13 @@
 export default {
   'translation-recent': [],
   'translation-picked': [],
+  'translation-sources': [],
 } as TranslationData;
 
 export interface TranslationData {
   'translation-recent': ListItem[] | object[] | [];
   'translation-picked': ListItem[] | object[] | [];
-  'translation-sources': Source[];
+  'translation-sources': Source[] | object[] | [];
 }
 
 interface Source {
---
+++ src/defaults/sources/google.ts
@@ -0,0 +1,42 @@
+import { SourcePreset } from './';
+
+export default (): string => JSON.stringify({
+  id: 'google',
+  name: 'Google',
+  url: 'https://translate.google.com',
+  query: {
+    text: {
+      method: 'GET',
+      url: '{{url}}/translate_a/single',
+      params: {
+        q: '{{q}}',
+        sl: '{{from}}',
+        tl: '{{to}}',
+        hl: '{{to}}',
+        client: 'gtx',
+        ie: 'UTF-8',
+        oe: 'UTF-8',
+        dt: ['bd', 'rm', 't'],
+        dj: '1',
+        source: 'icon',
+      },
+    },
+    audio: {
+      method: 'GET',
+      url: '{{url}}/translate_a/translate_tts',
+      params: {
+        q: '{{q}}',
+        tl: '{{from}}',
+        client: 'gtx',
+        ie: 'UTF-8',
+      },
+    }
+  },
+  parser: {
+    phoneticSrc: 'sentences.$.src_translit',
+    phoneticDest: 'sentences.$.translit',
+    translation: 'sentences(trans)',
+    explain: 'dict(pos....terms)',
+  },
+  fromto: ['auto', 'auto'],
+} as SourcePreset);

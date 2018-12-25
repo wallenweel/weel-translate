b7b025cae18638b57d5b6782be75e579e18ce20a
wallen <wallen_weel@outlook.com>
2018-12-25 19:49:10 +0800

    chore: test content script with shadow dom

---
+++ src/pages/Content.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="content">
+    <translation-result :result="result" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import TranslationResult from '@/components/TranslationResult.vue';
+
+@Component({
+  components: {
+    TranslationResult,
+  },
+})
+export default class Content extends Vue {
+  private result: SourcePreset['parser'] = {
+    phonetic_src: 'transˈlāSHən',
+    phonetic_dest: 'Fan Yi',
+    translation: '翻译',
+    explain: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.`,
+  };
+}
+</script>
+
+<style lang="scss">
+// @import '~vue-mdc-adapter/dist/vue-mdc-adapter.min.css';
+
+$mdc-theme-primary: #6200ee;
+$mdc-theme-secondary: #6200ee;
+$mdc-typography-font-family: "Roboto Mono", "Microsoft Yahei", "sans-serif", monospace;
+
+@import 'vue-mdc-adapter/dist/theme/styles';
+@import 'vue-mdc-adapter/dist/typography/styles';
+@import 'vue-mdc-adapter/dist/ripple/styles';
+@import 'vue-mdc-adapter/dist/icon/styles';
+
+@import '~vue-mdc-adapter/dist/layout-app/layout-app.min.css';
+@import '~vue-mdc-adapter/dist/icon-toggle/icon-toggle.min.css';
+@import '~vue-mdc-adapter/dist/button/button.min.css';
+@import '~vue-mdc-adapter/dist/fab/fab.min.css';
+@import '~vue-mdc-adapter/dist/card/card.min.css';
+@import '~vue-mdc-adapter/dist/linear-progress/linear-progress.min.css';
+</style>
--- src/pages/content.ts
+++ src/pages/content.ts
@@ -1,11 +1,35 @@
-// const div = document.createElement('div');
-// div.textContent = 'TEST';
+import Vue, { VueConstructor } from 'vue';
 
-// const mount = document.createElement('div');
-// document.body.appendChild(mount);
+Vue.config.productionTip = false;
 
-// const style = document.createElement('style');
-// style.textContent = `* { color: red; }`;
-// const shadow = mount.attachShadow({ mode: 'closed' });
-// shadow.appendChild(style);
-// shadow.appendChild(div);
+import VueMDCAdapter from 'vue-mdc-adapter';
+Vue.use(VueMDCAdapter);
+
+import i18n from '@/i18n';
+
+import App from './Content.vue';
+
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+import browser from '@/apis/browser';
+const port: RuntimePort = browser.runtime.connect({
+  name: 'port-from-popup',
+});
+
+const app = new Vue({
+  i18n,
+  render: (h) => h(App as VueConstructor),
+});
+
+((isDevelopment) => {
+  if (isDevelopment) { app.$mount('#app'); return; }
+
+  /** content shadow dom */
+  const wrap = document.createElement('weel-translate-x');
+  document.body.appendChild(wrap);
+
+  const shadow = wrap.attachShadow({ mode: 'open' });
+  shadow.appendChild(document.createElement('div'));
+
+  app.$mount(shadow.firstElementChild!);
+})(RUNTIME_ENV === 'development');

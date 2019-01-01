94d81a8c510e848dc7396e8174c3710d1a3e4f19
wallen <wallen_weel@outlook.com>
2019-01-01 15:05:14 +0800

    chore: add options page

---
+++ src/pages/Options.vue
@@ -0,0 +1,14 @@
+<template>
+  <mdc-layout-app>
+    {{ 'options page' }}
+  </mdc-layout-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Watch } from 'vue-property-decorator';
+
+@Component
+export default class Popup extends Vue {
+}
+</script>
--- src/pages/options.ts
+++ src/pages/options.ts
@@ -1,2 +1,43 @@
-// tslint:disable-next-line:no-console
-console.log('in options');
+import Vue, { VueConstructor } from 'vue';
+
+Vue.config.productionTip = false;
+
+import VueMDCAdapter from 'vue-mdc-adapter';
+Vue.use(VueMDCAdapter);
+
+import MdcAdapterPatch from '@/plugins/mdc-adapter-patch';
+Vue.use(MdcAdapterPatch);
+
+import GlobalComponents from '@/plugins/global-components';
+Vue.use(GlobalComponents);
+
+import Scrollbar from '@/plugins/scrollbar';
+Vue.use(Scrollbar);
+
+// import router from '@/routers/options';
+// import store from '@/stores/options';
+import i18n from '@/i18n';
+
+import App from './Options.vue';
+
+import browser from '@/apis/browser';
+const port: RuntimePort = browser.runtime.connect({
+  name: 'port-from-options',
+});
+
+// store.dispatch('init', { port }).then(() => {
+//   i18n.locale = store.getters.locale;
+//   store.watch(() => store.getters.locale, () => {
+//     i18n.locale = store.getters.locale;
+//   });
+// });
+
+new Vue({
+  // router,
+  // store,
+  i18n,
+  render: (h) => h(App as VueConstructor),
+}).$mount('#app');
+
+// import debug from '@/functions/debug';
+// debug.log(TARGET_BROWSER);
